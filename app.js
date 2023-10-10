42b5d944f8b2a7aefce715708c6c594714bbfd3e
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-11 01:09:01 +0200

    fixing

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ app.use('/email', emailRoutes);
 // Use the bookingRoutes router for booking-related endpoints
 app.use('/bookings', bookingRoutes);
 
-//Base path to the angular dist folder
-app.use(express.static(path.join(__dirname, 'dist/casa-rural')))
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname, 'dist/casa-rural/index.html'))
-})
-
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor en ejecución en el puerto ' + (process.env.PORT || 3000));
 });
